5c9fbc786987a148f398f1d74a4a225e50562ca1
cas <caswenting@gmail.com>
2024-09-13 15:47:57 +0200

    begin code mastermind

---
+++ MasterMind/src/mastermind/MasterMind.java
@@ -0,0 +1,135 @@
+package mastermind;
+
+public class MasterMind {
+
+	public static void main(String[] args) {
+
+		String wit = "wi";
+		String zwart = "zw";
+		String rood = "ro";
+		String blauw = "bl";
+		String groen = "gr";
+		String geel = "ge";
+		String paars = "pa";
+		String oranje = "or";
+		
+		String code1 = "wit";
+		String code2 = "";
+		String code3 = "";
+		String code4 = "";
+		
+		
+		String codebreaker1_1 = " ";
+		String codebreaker1_2 = " ";
+		String codebreaker1_3 = " ";
+		String codebreaker1_4 = " ";
+		
+		String codemaker1_1 = " ";
+		String codemaker1_2 = " ";
+		String codemaker1_3 = " ";
+		String codemaker1_4 = " ";
+		
+		
+		String codebreaker2_1 = " ";
+		String codebreaker2_2 = " ";
+		String codebreaker2_3 = " ";
+		String codebreaker2_4 = " ";
+		
+		String codemaker2_1 = " ";
+		String codemaker2_2 = " ";
+		String codemaker2_3 = " ";
+		String codemaker2_4 = " ";
+		
+		
+		String codebreaker3_1 = " ";
+		String codebreaker3_2 = " ";
+		String codebreaker3_3 = " ";
+		String codebreaker3_4 = " ";
+		
+		String codemaker3_1 = " ";
+		String codemaker3_2 = " ";
+		String codemaker3_3 = " ";
+		String codemaker3_4 = " ";
+		
+		
+		String codebreaker4_1 = " ";
+		String codebreaker4_2 = " ";
+		String codebreaker4_3 = " ";
+		String codebreaker4_4 = " ";
+		
+		String codemaker4_1 = " ";
+		String codemaker4_2 = " ";
+		String codemaker4_3 = " ";
+		String codemaker4_4 = " ";
+		
+		
+		String codebreaker5_1 = " ";
+		String codebreaker5_2 = " ";
+		String codebreaker5_3 = " ";
+		String codebreaker5_4 = " ";
+		
+		String codemaker5_1 = " ";
+		String codemaker5_2 = " ";
+		String codemaker5_3 = " ";
+		String codemaker5_4 = " ";
+		
+		
+		String codebreaker6_1 = " ";
+		String codebreaker6_2 = " ";
+		String codebreaker6_3 = " ";
+		String codebreaker6_4 = " ";
+		
+		String codemaker6_1 = " ";
+		String codemaker6_2 = " ";
+		String codemaker6_3 = " ";
+		String codemaker6_4 = " ";
+		
+		
+		String codebreaker7_1 = " ";
+		String codebreaker7_2 = " ";
+		String codebreaker7_3 = " ";
+		String codebreaker7_4 = " ";
+		
+		String codemaker7_1 = " ";
+		String codemaker7_2 = " ";
+		String codemaker7_3 = " ";
+		String codemaker7_4 = " ";
+		
+		
+		String codebreaker8_1 = " ";
+		String codebreaker8_2 = " ";
+		String codebreaker8_3 = " ";
+		String codebreaker8_4 = " ";
+		
+		String codemaker8_1 = " ";
+		String codemaker8_2 = " ";
+		String codemaker8_3 = " ";
+		String codemaker8_4 = " ";
+		
+		
+		String codebreaker9_1 = " ";
+		String codebreaker9_2 = " ";
+		String codebreaker9_3 = " ";
+		String codebreaker9_4 = " ";
+		
+		String codemaker9_1 = " ";
+		String codemaker9_2 = " ";
+		String codemaker9_3 = " ";
+		String codemaker9_4 = " ";
+		
+		
+		String codebreaker10_1 = " ";
+		String codebreaker10_2 = " ";
+		String codebreaker10_3 = " ";
+		String codebreaker10_4 = " ";
+		
+		String codemaker10_1 = " ";
+		String codemaker10_2 = " ";
+		String codemaker10_3 = " ";
+		String codemaker10_4 = " ";
+		
+
+		
+	}
+
+}
